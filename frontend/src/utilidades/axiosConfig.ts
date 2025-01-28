5f9e621dad2d9cfc805918f11d42f096ea2ce0a3
Jeremias Rivelli <jeremias.j.riv@gmail.com>
2025-01-28 18:03:10 -0300

    cambios al axios

--- frontend/src/utilidades/axiosConfig.ts
+++ frontend/src/utilidades/axiosConfig.ts
@@ -9,9 +9,12 @@ const api = axios.create({
 
 // Interceptor para inyectar el accessToken
 api.interceptors.request.use((config) => {
-  const accessToken = localStorage.getItem("accessToken");
-  if (accessToken) {
-    config.headers.Authorization = `Bearer ${accessToken}`;
+  const publicEndpoints = ["/api/roles"];
+  if (!publicEndpoints.some((endpoint) => config.url?.includes(endpoint))) {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
   }
   return config;
 });
