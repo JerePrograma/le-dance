79d5fa00b8f3b27c226c13935bbbdb860a1352de
Jeremias Rivelli <jeremias.j.riv@gmail.com>
2025-03-24 09:58:37 -0300

    migracion 44 correcta

--- backend/src/main/resources/db/migration/V44__alter-table-matricula.sql
+++ backend/src/main/resources/db/migration/V44__alter-table-matricula.sql
@@ -1,1 +1,11 @@
-ALTER TABLE matriculas ADD CONSTRAINT uq_alumno_anio UNIQUE (alumno_id, anio);
+WITH duplicates AS (SELECT id,
+                           ROW_NUMBER() OVER (PARTITION BY alumno_id, anio ORDER BY id) AS rn
+                    FROM matriculas)
+DELETE
+FROM matriculas
+WHERE id IN (SELECT id
+             FROM duplicates
+             WHERE rn > 1);
+
+ALTER TABLE matriculas
+    ADD CONSTRAINT uq_alumno_anio UNIQUE (alumno_id, anio);
